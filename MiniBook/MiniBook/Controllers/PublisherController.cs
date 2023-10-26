c15b2ccc339355099d1d3a5b59f629e36d1447aa
k-18123 <nguyenkhang181203@gmail.com>
2023-10-26 14:20:17 +0700

    Tạo và thêm db vào web
    Chia việc cho các thành viên có liên quang

---
+++ MiniBook/MiniBook/Controllers/PublisherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniBook.Controllers
+{
+    public class PublisherController : Controller
+    {
+        // GET: Publisher
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
